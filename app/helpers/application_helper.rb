d5c2b6e3d7d08441bc155155431d063c5adab45c
Olly <ollydutton@hotmail.com>
2014-03-25 20:38:04 +1100

    amended edit views for admin only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,36 +1,30 @@
 module ApplicationHelper
   def intellinav
-     links = ''
-     if @current_user
+      links = ''
+      if @current_user && @current_user.admin
+      links += "<li>#{ link_to('Users', users_path) }</li>
+                <li>#{ link_to('Photos', photos_path) }</li>
+                <li>#{ link_to('Add User', new_user_path) }</li>
+                <li>#{ link_to('Add Property', new_property_path) }</li>"
+      end
+
+      if @current_user
         links += "<li>#{ link_to('Properties', properties_path) }</li>
                 <li>#{ link_to('Arbitrators', arbitrators_path) }</li>
                 <li>#{ link_to('Offer Arbitration', new_arbitrator_path) }</li>
                 <li>#{ link_to('About', pages_about_path) }</li>
                 <li>#{ link_to('FAQ', pages_faq_path) }</li>
                 <li>#{ link_to('Logout, ' + @current_user.name, login_path, :method => :delete, :confirm => 'Comfirm Logout') }</li>"
-     else
-        links += "<li>#{ link_to('Sign up', new_user_path) }</li>
-          <li>#{ link_to('Sign in', login_path) }</li>"
-     end
-
-     if @current_user && @current_user.admin
-      links += "<li>#{ link_to('Users', users_path) }</li>
-                <li>#{ link_to('Add User', new_user_path) }</li>
-                <li>#{ link_to('Add Property', new_property_path) }</li>"
-       end
+      else
+        links += "<li>#{ link_to('Properties', properties_path) }</li>
+                <li>#{ link_to('Arbitrators', arbitrators_path) }</li>
+                <li>#{ link_to('About', pages_about_path) }</li>
+                <li>#{ link_to('FAQ', pages_faq_path) }</li>
+                <li>#{ link_to('Sign up', new_user_path) }</li>
+                <li>#{ link_to('Sign in', login_path) }</li>"
+      end
     links
   end
 end
 
-                # <li><%= link_to('Home', root_path) %></li>
-                # <li><%= link_to('Properties', properties_path) %></li>
-                # <li><%= link_to('Add Property', new_property_path) %></li>
-                # <li><%= link_to('Users', users_path) %></li>
-                # <li><%= link_to('Add User', new_user_path) %></li>
-                # <li><%= link_to('Arbitrators', arbitrators_path) %></li>
-                # <li><%= link_to('Offer Arbitration', new_arbitrator_path) %></li>
-                # <li><%= link_to('Images', photos_path) %></li>
-                # <li><%= link_to('About', pages_about_path) %></li>
-                # <li><%= link_to('FAQ', pages_faq_path) %></li>
-                # <li><%= link_to('Sellers', new_user_path) %></li>
-                # <li><%= link_to('Contact', pages_contact_path) %></li>
+      
