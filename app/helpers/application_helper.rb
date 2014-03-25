f2cd1140ba92d8c169a68d747c2e51fd5456978d
Olly <ollydutton@hotmail.com>
2014-03-25 16:11:04 +1100

    deleted image from form helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,15 @@ module ApplicationHelper
   def intellinav
      links = ''
      if @current_user
-      links += "<li>#{ link_to('Properties', properties_path) }</li>
+        links += "<li>#{ link_to('Properties', properties_path) }</li>
                 <li>#{ link_to('Arbitrators', arbitrators_path) }</li>
                 <li>#{ link_to('Offer Arbitration', new_arbitrator_path) }</li>
                 <li>#{ link_to('About', pages_about_path) }</li>
                 <li>#{ link_to('FAQ', pages_faq_path) }</li>
                 <li>#{ link_to('Logout, ' + @current_user.name, login_path, :method => :delete, :confirm => 'Comfirm Logout') }</li>"
      else
-      links += "<li>#{ link_to('Sign up', new_user_path) }</li>
-       <li>#{ link_to('Sign in', login_path) }</li>"
+        links += "<li>#{ link_to('Sign up', new_user_path) }</li>
+          <li>#{ link_to('Sign in', login_path) }</li>"
      end
 
      if @current_user && @current_user.admin
